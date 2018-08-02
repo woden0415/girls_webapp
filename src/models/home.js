2f00f9be76cbdfa268bca1227cfb08c8880a984f
wang_donga <wang_donga@Ctrip.com>
2018-08-03 00:47:38 +0800

    Revert "Revert "添加图标，并实现点击导航切换内容""
    This reverts commit 2c7e353b0042e38a86fc5ae2bd5d6911d30c6e53.

--- src/models/home.js
+++ src/models/home.js
@@ -11,7 +11,14 @@ export default {
 
   state: {
     name:'这是home的model',
-    arrImgUrls: []
+    arrImgUrls: [],
+    arrNav: [
+      { icon: false, name: '首页', },
+      { icon: false, name: '关注' },
+      { icon: false, name: '消息' },
+      { icon: false, name: '我' }
+    ],
+    activeTab: 0
   },
 
   subscriptions: {
@@ -19,21 +26,57 @@ export default {
   },
 
   effects: {
+    // 获取图片数据
     * fetchImgsUrl({ payload }, { call, put }) {
-
       const response = yield call(axios.get, imgList, payload);
       yield put({
         type: 'fetchImgsUrlFn',
         payload: response.data.list,
       });
+      yield put({
+        type: 'changeFootIconFn',
+        payload: undefined
+      })
+
     },
+
+    // 改变按钮刷新状态
+    * changeFootIcon({ payload }, {call, put}) {
+      let { index } = payload;
+      yield put({
+        type: 'changeFootIconFn',
+        payload: index,
+      })
+    }
   },
 
   reducers: {
+    // 获取图片数据
     fetchImgsUrlFn(state, action) {
       return {
         ...state,
         arrImgUrls: action.payload
+      }
+    },
+
+    // 改变按钮刷新状态
+    changeFootIconFn(state, action){
+      let { arrNav } = state;
+      let arrTmp = JSON.parse(JSON.stringify(arrNav));
+      if (action.payload !== undefined) {
+        for (let i = 0; i < arrTmp.length; i++) {
+          if ( i === action.payload ) {
+            arrTmp[i].icon = true
+          }
+        }
+      } else {
+        for (let i = 0; i < arrTmp.length; i++) {
+          arrTmp[i].icon = false
+        }
+      }
+      return {
+        ...state,
+        arrNav: arrTmp
       }
     }
   },
